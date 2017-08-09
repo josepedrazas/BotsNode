b6accdc25f1ae9e04fed8525b3a290a74ee5a9a3
Jose Pedraza <josepe.pedraza@gmail.com>
2017-08-09 18:00:42 -0400

    QuitandoServer

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-var restify = require('restify');
 var builder = require('botbuilder');
-
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978,function(){
-    console.log('%s listening to %s', server.name, server.url);
-});
-
 
 var connector = new builder.ChatConnector({
     appId: '',
@@ -13,7 +6,6 @@ var connector = new builder.ChatConnector({
 });
 
 var bot = new builder.UniversalBot(connector);
-server.post('/api/messages',connector.listen());
 
 bot.dialog('/', [
     function(session)
