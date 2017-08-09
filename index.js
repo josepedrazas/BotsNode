2d17cfa6b17bf2112f8c0c539a670f35def377a4
Jose Pedraza <josepe.pedraza@gmail.com>
2017-08-09 18:41:18 -0400

    LoadServer

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+var restify = require('restify');
 var builder = require('botbuilder');
+
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978,function(){
+    console.log('%s listening to %s', server.name, server.url);
+});
+
 
 var connector = new builder.ChatConnector({
     appId: 'd507dcbf-435d-4ff7-a97f-c504f6e19ab7',
@@ -6,6 +13,7 @@ var connector = new builder.ChatConnector({
 });
 
 var bot = new builder.UniversalBot(connector);
+server.post('/api/messages',connector.listen());
 
 bot.dialog('/', [
     function(session)
